b5e9343584cfcddd9b5948a66ac50d023c419d8d
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2011-08-06 14:31:22 +0200

    addded swipe-gestures for page turning

--- src/de/deepsource/deepnotes/activities/listener/DrawTouchListener.java
+++ src/de/deepsource/deepnotes/activities/listener/DrawTouchListener.java
@@ -10,6 +10,10 @@ public class DrawTouchListener implements View.OnTouchListener {
 	private boolean multiTouch = false;
 	private DrawView drawView;
 	private DrawActivity drawActivity;
+	
+	// TODO: use % of display width
+	private float swipeTrigger = 100f;
+	private float swipeXdelta = 0f;
 
 	public DrawTouchListener(DrawActivity drawActivity, DrawView drawView) {
 		this.drawView = drawView;
@@ -17,20 +21,37 @@ public class DrawTouchListener implements View.OnTouchListener {
 	}
 
 	public boolean onTouch(View v, MotionEvent event) {
-
+		// Checking for multiTouch
 		if (event.getPointerCount() > 1) {
+			switch (event.getAction()) {
+			// 2-finger click, storing first coordinate
+			case (MotionEvent.ACTION_DOWN):
+				swipeXdelta = event.getX(0);
+				break;
 			
-			return true;
-		}
+			// 2-finger swipe, calculating direction
+			case (MotionEvent.ACTION_MOVE):
+				if(Math.abs(swipeXdelta-event.getX(0)) > swipeTrigger) {
+					if(swipeXdelta < event.getX(0)){
+						// swipe left
+						drawActivity.showPreviousDrawView();
+					}else{
+						// swipe right
+						drawActivity.showNextDrawView();
+					}
+				}
+				break;
+			}
+		} else {
+			switch (event.getAction()) {
+			case (MotionEvent.ACTION_UP):
+				drawView.addPoint(-1f, -1f);
+				break;
 
-		switch (event.getAction()) {
-		case (MotionEvent.ACTION_UP):
-			drawView.addPoint(-1f, -1f);
-			break;
-
-		case (MotionEvent.ACTION_DOWN):
-		case (MotionEvent.ACTION_MOVE):
-			drawView.addPoint(event.getX(), event.getY());
+			case (MotionEvent.ACTION_DOWN):
+			case (MotionEvent.ACTION_MOVE):
+				drawView.addPoint(event.getX(), event.getY());
+			}
 		}
 
 		return true;
